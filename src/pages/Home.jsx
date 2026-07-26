337b8fc8a20aa5cc97279c13cbe6728b5eb7b49e
iamsagarsavita-cod <iamsagarsavita@gmail.com>
2026-07-26 10:49:47 +0530

    created all the basic needed elements

---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+function home(){
+    return(
+        <div className="mx-auto py-10">
+            <h1 className="text-4xl font-bold text-center mb-6">Welcome To News Portal</h1>
+            <p className="text-center text-gray-600">Read the latest news from around the world</p>
+        </div>
+    )
+}
