384a64e904345db102f83773941a728c36382476
iamsagarsavita-cod <iamsagarsavita@gmail.com>
2026-07-26 11:30:57 +0530

    created a Navbar successfully

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,12 @@ import Navbar from "./components/Navbar";
 function App(){
   return(
     <>
+    <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<NotFound />} />
-
 
 
       </Routes>
